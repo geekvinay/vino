5b61f7bbb084d7cca52541b5ae606f447991d31a
geekvinay <geekvinay002@gmail.com>
2022-01-18 22:24:37 +0530

    commit changes

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,6 +3,8 @@ import './style.css'
 
 const allSections = document.querySelectorAll('.section');
 
+// Buttery Smooth 💫 Reveal Animation
+
 const revealSection = function (entries, observer) {
     const [entry] = entries;
 
@@ -20,3 +22,55 @@ allSections.forEach(function (section) {
     sectionObserver.observe(section);
     section.classList.add('section--hidden');
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img');
+    });
+
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+
+// Lazy loading for Backgrounds
+(() => {
+    const imgTargets = document.querySelectorAll('img[data-background]');
+
+    const loadImg = function (entries, observer) {
+        const [entry] = entries;
+
+        if (!entry.isIntersecting) return;
+
+        // Replace src with data-src
+        entry.target.src = entry.target.dataset.background;
+        entry.target.addEventListener('load', function () {
+            entry.target.classList.remove('lazy-img');
+        });
+        observer.unobserve(entry.target);
+    };
+
+    const imgObserver = new IntersectionObserver(loadImg, {
+        root: null,
+        threshold: 0,
+        rootMargin: '200px',
+    });
+    imgTargets.forEach(img => imgObserver.observe(img));
+})();
